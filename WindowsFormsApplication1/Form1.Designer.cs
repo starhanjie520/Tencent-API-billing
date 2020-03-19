3d5bb90453387ae0a2be943dcdf7212f33b90a4e
starhanjie520 <252033592@qq.com>
2020-03-19 09:53:01 +0900

    1
    1

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,178 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.checkedListBox1 = new System.Windows.Forms.CheckedListBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.groupBox3 = new System.Windows.Forms.GroupBox();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.groupBox3.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(27, 28);
+            this.button1.Margin = new System.Windows.Forms.Padding(6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(149, 80);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("굴림", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(214, 28);
+            this.button2.Margin = new System.Windows.Forms.Padding(6);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(143, 80);
+            this.button2.TabIndex = 5;
+            this.button2.Text = "Stop";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // checkedListBox1
+            // 
+            this.checkedListBox1.FormattingEnabled = true;
+            this.checkedListBox1.Location = new System.Drawing.Point(13, 78);
+            this.checkedListBox1.Name = "checkedListBox1";
+            this.checkedListBox1.Size = new System.Drawing.Size(266, 532);
+            this.checkedListBox1.TabIndex = 6;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Location = new System.Drawing.Point(18, 43);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(72, 16);
+            this.checkBox1.TabIndex = 7;
+            this.checkBox1.Text = "모두선택";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.CustomFormat = "yyyy 년 MM 월";
+            this.dateTimePicker1.Format = System.Windows.Forms.DateTimePickerFormat.Custom;
+            this.dateTimePicker1.Location = new System.Drawing.Point(46, 34);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.ShowUpDown = true;
+            this.dateTimePicker1.Size = new System.Drawing.Size(208, 21);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.dateTimePicker1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 654);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(292, 87);
+            this.groupBox1.TabIndex = 12;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "기간 설정";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.checkBox1);
+            this.groupBox2.Controls.Add(this.checkedListBox1);
+            this.groupBox2.Location = new System.Drawing.Point(12, 12);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(292, 627);
+            this.groupBox2.TabIndex = 13;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "업체";
+            // 
+            // groupBox3
+            // 
+            this.groupBox3.Controls.Add(this.button1);
+            this.groupBox3.Controls.Add(this.button2);
+            this.groupBox3.Location = new System.Drawing.Point(410, 12);
+            this.groupBox3.Name = "groupBox3";
+            this.groupBox3.Size = new System.Drawing.Size(380, 129);
+            this.groupBox3.TabIndex = 14;
+            this.groupBox3.TabStop = false;
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 12;
+            this.listBox1.Location = new System.Drawing.Point(321, 147);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(552, 604);
+            this.listBox1.TabIndex = 15;
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(911, 773);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.groupBox3);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(6);
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "CAPCLOUD";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.groupBox3.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.CheckedListBox checkedListBox1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.GroupBox groupBox3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,307 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using TencentCloud.Common;
+using TencentCloud.Common.Profile;
+using TencentCloud.Billing.V20180709;
+using TencentCloud.Billing.V20180709.Models;
+using ClosedXML;
+using ClosedXML.Excel;
+using Newtonsoft.Json.Linq;
+using System.Text.RegularExpressions;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+
+        private Thread main_Thread = null;
+        public String path = Application.StartupPath + "\\" ;
+        public String[] tt = null;
+        public int allCompanyIndex = 1;
+
+        public String Company = "";
+        public String UIN = "";
+        public String SecretIdfromeFile = "";
+        public String SecretKeyfromeFile = "";
+        public String CVMRegion = "ap-seoul";  //ap-beijing
+        public String BeginTime = "";
+        public String EndTime = "";
+        public String fileName = "";
+        public int exelIndex = 2;
+        public String months = "";
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            StreamReader sr = new StreamReader("업체.txt", Encoding.UTF8);
+            String rrr = sr.ReadToEnd();
+            sr.Close();
+            tt = Regex.Split(rrr, "\r\n");
+            for (int i = 0; i < tt.Count(); i++)
+            {
+                String CompanyStr = Regex.Split(tt[i], "	")[0];
+                checkedListBox1.Items.Add(CompanyStr);
+            }
+
+
+            if (checkBox1.Checked)
+            {
+                for (int j = 0; j < checkedListBox1.Items.Count; j++)
+                {
+                    checkedListBox1.SetItemChecked(j, true);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < checkedListBox1.Items.Count; j++)
+                {
+                    checkedListBox1.SetItemChecked(j, false);
+                }
+            }
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            main_Thread = new Thread(myStaticThreadMethod);
+            main_Thread.Start();
+
+        }
+
+        public void myStaticThreadMethod()
+        {
+            Test();
+            MessageBox.Show("CAPCLOUD 오늘 하루도 수고 하셨습니다. ^_^ ");
+            System.Windows.Forms.Application.Exit();
+        }
+
+
+        public void Test()
+        {
+            int createFileIndex = 1;
+            string[] allFiles = Directory.GetFiles(path, "*.xlsx");
+            foreach (string strFile in allFiles)
+            {
+                File.Delete(strFile);
+            } 
+            ListBoxItemAdd(this, this.listBox1, "프로그램 시작 합니다. ^_^");
+            foreach (int indexChecked in checkedListBox1.CheckedIndices)
+            {
+                String lineStr = tt[indexChecked].ToString();
+                Console.WriteLine(lineStr);
+                try
+                {
+                    string[] sArray = lineStr.Split('	');
+                    if (sArray.Count() < 2)                                      //작업완료 판단
+                    {
+                        break;
+                    }
+
+                    Company = sArray[0];
+                    UIN = sArray[1].Replace(" ","");
+                    SecretIdfromeFile = sArray[2].Replace(" ", "");
+                    SecretKeyfromeFile = sArray[3].Replace(" ", "");
+                    SecretKeyfromeFile = sArray[3].Replace(" ", "");
+                    CVMRegion  = sArray[4].Replace(" ", "");
+
+                    BeginTime = dateTimePicker1.Value.ToString("yyyy-MM-01 00:00:00");
+                    EndTime = dateTimePicker1.Value.ToString("yyyy-MM-01 00:00:01");
+                    months = dateTimePicker1.Value.ToString("yyyy년MM월");
+                    Console.WriteLine(Company);
+                    Console.WriteLine(UIN);
+                    Console.WriteLine(SecretIdfromeFile);
+                    Console.WriteLine(SecretKeyfromeFile);
+                    Console.WriteLine(BeginTime);
+                    Console.WriteLine(EndTime);               
+                    Thread.Sleep(1000);
+                    ListBoxItemAdd(this, this.listBox1, "======<< 업체명 : " + Company + " >>=====");
+
+                    fileName = createFileIndex + "-" + Company + "-" + months + ".xlsx";   //파일생성
+                    WriteExel(fileName, 1, 1, "TotalCost");
+                    WriteExel("모든업체-" + months + ".xlsx", allCompanyIndex, 1, Company);
+
+                    Credential cred = new Credential
+                    {
+                        SecretId = SecretIdfromeFile,
+                        SecretKey = SecretKeyfromeFile
+                    };
+
+                    ClientProfile clientProfile = new ClientProfile();
+                    HttpProfile httpProfile = new HttpProfile();
+                    httpProfile.Endpoint = ("billing.tencentcloudapi.com");
+                    clientProfile.HttpProfile = httpProfile;
+                    BillingClient client = new BillingClient(cred, CVMRegion, clientProfile);
+                    DescribeBillSummaryByProductRequest req = new DescribeBillSummaryByProductRequest();
+                    string strParams = "{\"PayerUin\":\"" + UIN + "\",\"BeginTime\":\"" + BeginTime + "\",\"EndTime\":\"" + EndTime + "\"}";
+                    req = DescribeBillSummaryByProductRequest.FromJsonString<DescribeBillSummaryByProductRequest>(strParams);
+                    DescribeBillSummaryByProductResponse resp = client.DescribeBillSummaryByProduct(req).ConfigureAwait(false).GetAwaiter().GetResult();
+                    //  Console.WriteLine(AbstractModel.ToJsonString(resp));
+                    String ttt = AbstractModel.ToJsonString(resp);
+
+                    String total = GetSummaryTotal(ttt);                 
+                    WriteExel(fileName, 1, 2, total);                    
+                    WriteExel("모든업체-" + months + ".xlsx", allCompanyIndex, 2, total);
+                    allCompanyIndex++;
+
+                    ListBoxItemAdd(this, this.listBox1, "Total : =》  " + total);
+
+                    Dictionary<string, string> Overview = GetSummaryOverview(ttt);
+                    exelIndex = 2;
+                    foreach (var item in Overview)
+                    {
+                        Console.WriteLine(item.Key + item.Value);
+                        WriteExel(fileName, exelIndex, 1, item.Key);
+                        WriteExel(fileName, exelIndex, 2, item.Value);
+                        exelIndex++;
+                        ListBoxItemAdd(this, this.listBox1, item.Key + "  --->    " +item.Value);
+                    }
+                    createFileIndex++;
+                    Thread.Sleep(100);
+                }
+                catch (Exception e)
+                {
+                    //WriteExel(fileName, exelIndex, 1, Company);
+                    WriteExel("모든업체-" + months + ".xlsx", allCompanyIndex, 2, "에러");
+                    createFileIndex++;
+                    ListBoxItemAdd(this, this.listBox1, "<< 업체명 : " + Company + " >> 요금 실패~~");
+                }
+                ListBoxItemAdd(this, this.listBox1, " ");
+                ListBoxItemAdd(this, this.listBox1, " ");
+            
+            }
+
+
+        }
+
+        public static string GetSummaryTotal(string jsonText)
+        {
+            JObject jsonObj = JObject.Parse(jsonText);
+            String a = jsonObj["SummaryTotal"]["RealTotalCost"].ToString();
+            return a;
+        }
+        public static Dictionary<string, string> GetSummaryOverview(string jsonText)
+        {
+            Dictionary<string, string> dic = new Dictionary<string, string>();
+            JObject jsonObj = JObject.Parse(jsonText);
+            var a = jsonObj.SelectToken("SummaryOverview");
+            foreach (var item in a)
+            {
+                String BusinessCodeName = item.SelectToken("BusinessCodeName").ToString();
+                String RealTotalCost = item.SelectToken("RealTotalCost").ToString();
+                Console.WriteLine(BusinessCodeName + "-" + RealTotalCost);
+                dic.Add(BusinessCodeName, RealTotalCost);
+            }
+            return dic;
+        }
+        public void WriteExel(String fileName, int x, int y, String strValue)
+        {
+            if (!File.Exists(fileName))
+            {
+                var wb = new XLWorkbook();
+                var ws = wb.Worksheets.Add(fileName.Replace(".xlsx", ""));
+                IXLWorksheet sheet = wb.Worksheet(1);
+                sheet.Cell(x, y).Value = strValue;
+                wb.SaveAs(fileName);
+            }
+            else
+            {
+                var wb = new XLWorkbook(fileName);
+                IXLWorksheet sheet = wb.Worksheet(1);
+                sheet.Cell(x, y).Value = strValue;
+                wb.SaveAs(fileName);
+            }
+        }
+      
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            if (this.main_Thread != null)
+            {
+                this.main_Thread.Abort();
+                this.main_Thread = null;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            //button1.Enabled = true;
+            //button2.Enabled = false;
+            if (this.main_Thread != null)
+            {
+                this.main_Thread.Abort();
+                this.main_Thread = null;
+            }
+        }
+
+
+        
+
+      
+        public static void ListBoxItemAdd(Form frm, ListBox lstbox, string lstitem)
+        {
+            frm.Invoke(new MethodInvoker(delegate
+            {
+                if (lstbox.Items.Count + 1 > 200)
+                {
+                    lstbox.Items.RemoveAt(0);
+                }
+                lstbox.Items.Add(DateTime.Now.ToString("HH:mm:ss") + " | " + lstitem);
+                lstbox.SelectedIndex = lstbox.Items.Count - 1;
+                lstbox.Refresh();
+                //StreamWriter sw = new StreamWriter(Application.StartupPath + "\\Log.txt", true, Encoding.Default);
+                //sw.WriteLine(DateTime.Now.ToString("HH:mm:ss") + " | " + lstitem);
+                //sw.Close();
+            }));
+        }
+      
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            if (checkBox1.Checked)
+            {
+                for (int j = 0; j < checkedListBox1.Items.Count; j++)
+                {
+                    checkedListBox1.SetItemChecked(j, true);
+                }
+            }
+            else
+            {
+                for (int j = 0; j < checkedListBox1.Items.Count; j++)
+                {
+                    checkedListBox1.SetItemChecked(j, false);
+                }
+            }
+        }
+
+    }
+}
